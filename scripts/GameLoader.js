40eb56abbec7122bad90e0d1d4619ead50322bc6
Istanful <w.e.w@live.se>
2017-04-17 09:08:40 +0200

    Add basic functionality
    This includes making some of the base classes
    and implementing the game loader.

---
+++ scripts/GameLoader.js
@@ -0,0 +1,46 @@
+// Load all scripts, firing callback when each script is done
+function loadScripts(scripts) {
+  var i = 0;
+  var callback = function() {
+    if (i++ < scripts.length - 1) {
+      loadScript(scripts[i], callback);
+    }
+  }
+  // Load first script
+  loadScript(scripts[0], callback);
+}
+
+// Load script and fire callback when done
+function loadScript(fileName, callback) {
+  let script = document.createElement("script");
+  script.type = "text/javascript";
+
+  // IE detect script done
+  if (script.readyState) {
+    script.onreadystatechange = function() {
+      if (script.readyState == "loaded") {
+        script.readyState == "loaded";
+        script.onreadystatechange = null;
+        callback();
+      }
+    }
+  }
+  // All other browsers detect script done
+  else {
+    script.onload = function() {
+      callback();
+    }
+  }
+
+  script.src = "scripts/" + fileName;
+  document.getElementsByTagName("head")[0].append(script);
+}
+
+scripts = [
+  "GameObject.js",
+  "Vector.js",
+  "Game.js",
+  "GameController.js"
+];
+
+loadScripts(scripts);
---
+++ scripts/GameObject.js
@@ -0,0 +1,6 @@
+class GameObject {
+  constructor(name, position) {
+    this.name = name;
+    this.position = position;
+  }
+}
