fd2b851c5fb9047d934d787ebccc81be899294eb
Istanful <w.e.w@live.se>
2017-04-17 12:28:05 +0200

    Add update and draw
    These methods will be responsible for calling update and draw on each object.
    What is to be updated and drawed is up to the game object.

--- scripts/GameController.js
+++ scripts/GameController.js
@@ -1,1 +1,4 @@
 let game = new Game();
+let test = new GameObject("test", new Vector(1, 1 ));
+game.gameObjects.push(test);
+game.start();
--- scripts/GameObject.js
+++ scripts/GameObject.js
@@ -2,5 +2,16 @@ class GameObject {
   constructor(name, position) {
     this.name = name;
     this.position = position;
+    this.velocity = new Vector(1, 2);
+  }
+
+  update() {
+      this.position.x += this.velocity.x;
+      this.position.y += this.velocity.y;
+  }
+
+  draw(context) {
+    context.fillStyle = "black";
+    context.fillRect(this.position.x, this.position.y, 100, 100);
   }
 }
